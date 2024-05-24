b1ca223cce68c005830a998603b44f4c9d01fdf1
Shivzee <wizzard1608@gmail.com>
2024-05-24 14:57:59 +0000

    fixed zod config for component

--- parser/schema.ts
+++ parser/schema.ts
@@ -4,5 +4,5 @@ export const metadataSchema = z.object({
   title: z.string().min(1),
   description: z.string().min(5),
   category: z.string().min(1),
-  component: z.any(),
+  component: z.function(),
 });
