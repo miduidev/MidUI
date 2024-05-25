0365c68a4e9ef5fb48daa2de04a613f705808dfc
Shivzee <wizzard1608@gmail.com>
2024-05-25 20:36:08 +0000

    add docs

--- lib/emitter/docs.ts
+++ lib/emitter/docs.ts
@@ -4,13 +4,27 @@
  * only be made by miduidevs based on community feedback.
  * 
  * DO NOT UPDATE THIS FILE!
+ * 
+ * @shivam1608 @fero1xd @dexfadee
  */
 import GettingStarted from "@/data/docs/getting-started.mdx";
+import Installation from "@/data/docs/installation.mdx";
+import Contributing from "@/data/docs/contributing.mdx";
 
 export const contents = [
   {
     name: "Getting Started",
     component: GettingStarted,
     slugAsParams: "getting-started",
+  },
+  {
+    name: "Installation",
+    component: Installation,
+    slugAsParams: "installation",
+  },
+  {
+    name: "Contributing",
+    component: Contributing,
+    slugAsParams: "contributing",
   },
 ];
