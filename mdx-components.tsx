246ec1ebe3cbc33659cf4c44dcdc42d470fddbe4
Pranjal Butola <pranjalbutola12@gmail.com>
2024-05-26 12:02:14 +0530

    using next/image component

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,7 +1,15 @@
 import type { MDXComponents } from "mdx/types";
+import Image, { ImageProps } from "next/image";
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
+    img: (props) => (
+      <Image
+        sizes="100vw"
+        style={{ width: "100%", height: "auto" }}
+        {...(props as ImageProps)}
+      />
+    ),
     ...components,
   };
 }
