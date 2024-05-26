3f7922ed40898adf875a683d2dc34581ccc9bfc2
Pranjal Butola <pranjalbutola12@gmail.com>
2024-05-26 12:06:52 +0530

    exporting metadata for docs

--- app/docs/[slug]/page.tsx
+++ app/docs/[slug]/page.tsx
@@ -1,9 +1,42 @@
 import { contents } from "@/lib/emitter/docs";
+import { Metadata } from "next";
 import { notFound } from "next/navigation";
 
 type PageProps = {
   params: { slug: string };
 };
+
+export async function generateMetadata({
+  params,
+}: PageProps): Promise<Metadata> {
+  const c = contents.find((c) => c.slugAsParams === params.slug);
+  if (!c) {
+    return {};
+  }
+
+  const title = c.name + " - MidUI";
+  const description = `Read documentation at MidUI`;
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      type: "article",
+      images: [
+        {
+          url: `/api/${c.name}`,
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: title,
+      description: description,
+      images: [`/api/${c.name}`],
+    },
+  };
+}
 
 export async function generateStaticParams() {
   return contents.map((c) => ({
