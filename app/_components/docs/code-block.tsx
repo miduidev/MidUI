abd1a9bfb5334d5f7136a4d7ed307140d3e96458
Pranjal Butola <pranjalbutola12@gmail.com>
2024-05-29 12:30:31 +0530

    global language selector

--- app/_components/docs/code-block.tsx
+++ app/_components/docs/code-block.tsx
@@ -2,8 +2,9 @@
 
 import { cn } from "@/utils/cn";
 import path from "path-browserify";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import CodeSelect from "./code-select";
+import { useConfigStore } from "@/store/config";
 
 type CodeBlockProps = {
   files: string[];
@@ -11,8 +12,13 @@ type CodeBlockProps = {
 };
 
 export const CodeBlock = ({ files, codes }: CodeBlockProps) => {
-
-  const [active, setActive] = useState(0);
+  const lang = useConfigStore((config) => config.language);
+  const activeIndex = useMemo(() => {
+    for (let i = 0; i < files.length; i++)
+      if (path.extname(files[i]) === `.${lang}`) {
+        return i;
+      }
+  }, [lang]);
 
   return (
     <div className="flex flex-col">
@@ -23,17 +29,19 @@ export const CodeBlock = ({ files, codes }: CodeBlockProps) => {
           "flex items-center justify-between"
         )}
       >
-        <p className="font-medium text-[12px]">{path.basename(files[active])}</p>
+        <p className="font-medium text-[12px]">
+          {path.basename(files[activeIndex ?? 0])}
+        </p>
 
         <div className="flex items-center gap-[10px] justify-center">
-          <CodeSelect active={active} setActive={setActive} files={files} />
+          <CodeSelect files={files} activeIndex={activeIndex} />
         </div>
       </div>
 
       <div className="flex flex-col p-[16px] bg-[#121212] rounded-[10px] rounded-t-none">
         <div
           dangerouslySetInnerHTML={{
-            __html: codes[active],
+            __html: codes[activeIndex ?? 0],
           }}
         ></div>
       </div>
--- app/_components/docs/code-select.tsx
+++ app/_components/docs/code-select.tsx
@@ -1,27 +1,45 @@
 "use client";
-import { getCodeIcon } from '@/utils';
+import { useConfigStore } from "@/store/config";
+import { getCodeIcon } from "@/utils";
 import path from "path-browserify";
 
-const CodeSelect = ({active , setActive ,files} : {active : number , setActive : Function , files : string[]}) => {
+const CodeSelect = ({
+  files,
+  activeIndex,
+}: {
+  files: string[];
+  activeIndex: number | undefined;
+}) => {
+  const setLanguage = useConfigStore((config) => config.setLanguage);
 
-    const checkOnClick = (e : React.MouseEvent<HTMLButtonElement>) => {
-        e.preventDefault();
-        const target = e.target as HTMLButtonElement;
-        const id = target.getAttribute("midui-id");
-        if(id){
-            setActive(parseInt(id));
-        }
-    }
-
-    return (
+  return (
     <div className="w-full flex justify-center text-xs">
-        <div className="flex mx-3 bg-[#191919] w-fit rounded-md transition-colors duration-300">
-            {
-                files.map((v,i)=><button key={i} midui-id={i} onClick={checkOnClick} className={`${active === i &&"bg-[#1565C0]"}  px-2 py-1 rounded-md uppercase`}> <div className="flex items-center space-x-2 pointer-events-none"><span>{path.extname(v).slice(1)}</span> <span>{getCodeIcon(v)}</span></div> </button>)
-            }
-        </div>
+      <div className="flex mx-3 bg-[#191919] w-fit rounded-md transition-colors duration-300">
+        {files.map((v, i) => {
+          const ext = path.extname(v).slice(1);
+
+          return (
+            <button
+              key={i}
+              midui-id={i}
+              onClick={(e) => {
+                e.preventDefault();
+                setLanguage(ext as "tsx" | "html" | "vue");
+              }}
+              className={`${
+                (activeIndex ?? 0) === i && "bg-[#1565C0]"
+              }  px-2 py-1 rounded-md uppercase`}
+            >
+              <div className="flex items-center space-x-2 pointer-events-none">
+                <span>{ext}</span>
+                <span>{getCodeIcon(v)}</span>
+              </div>
+            </button>
+          );
+        })}
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default CodeSelect;
