c0ab1129774c01436011334bcbf43697e2fca08d
Shivzee <wizzard1608@gmail.com>
2024-06-03 14:11:45 +0000

    style scrollbar

--- app/_components/docs/side-nav.tsx
+++ app/_components/docs/side-nav.tsx
@@ -20,7 +20,7 @@ const SideNav = ({ className, input }: SideNavProps) => {
   return (
     <div className={cn("sticky top-0 h-max pt-10 shrink-0", className)}>
       {input}
-      <div className="max-h-[85vh] overflow-x-hidden overflow-y-auto pl-3">
+      <div className="max-h-[85vh] overflow-x-hidden overflow-y-hidden hover:overflow-y-auto pl-3 midui-scroll">
         <h2 className="mt-8 font-semibold text-[20px] mb-3">Documentation</h2>
         <div
           className={cn(
