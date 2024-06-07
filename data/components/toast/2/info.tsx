4a17336bb7991707418b1801c4529b4503890fe2
Fade <tryingtogetalifeoninternet@gmail.com>
2024-06-07 20:21:16 +0530

    add toast need to add functionality

---
+++ data/components/toast/2/info.tsx
@@ -0,0 +1,11 @@
+import { Metadata } from "@/lib/types";
+import { Toast } from "./example";
+
+const metadata = {
+  title: "Toast with Icon",
+  description: "An toast element with icon",
+  category: "toast",
+  component: Toast,
+} satisfies Metadata;
+
+export default metadata;
---
+++ data/components/toast/4/info.tsx
@@ -0,0 +1,11 @@
+import { Metadata } from "@/lib/types";
+import { Toast } from "./example";
+
+const metadata = {
+  title: "Success Toast with Fill",
+  description: "An Success toast element with fill",
+  category: "toast",
+  component: Toast,
+} satisfies Metadata;
+
+export default metadata;
