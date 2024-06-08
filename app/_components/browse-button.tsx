ea91deaadfaf6ce61e5a6548b18f7f9589172724
Shivzee <wizzard1608@gmail.com>
2024-06-08 20:07:42 +0000

    fix landing page button

--- app/_components/browse-button.tsx
+++ app/_components/browse-button.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 const BrowseButton = () => {
   return (
     <Link
-      href="/docs/"
+      href="/docs/components/"
       className="text-sm md:text-lg font-medium self-center md:self-start mt-8 px-3 md:px-5 py-2 md:py-3 flex items-center space-x-2 border-[#1565C0] border-2 rounded-full transition-all duration-150 hover:scale-105 hover:bg-[#1565C0]"
     >
       <span>Browse Components</span>
