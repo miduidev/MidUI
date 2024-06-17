911a4d15972d36ab97920fee8faad97fec6ffb1f
Pranjal Butola <pranjalbutola12@gmail.com>
2024-06-17 20:57:48 +0530

    parser check disabled temp

--- parser/parser.ts
+++ parser/parser.ts
@@ -1,16 +1,16 @@
-import fs from "fs";
-import path from "path";
-import { glob } from "glob";
-import { getHighlighter } from "shiki";
-import serialize from "serialize-javascript";
-import { Metadata } from "@/lib/types";
-import { v4 } from "uuid";
-import { metadataSchema } from "./schema";
+import fs from 'fs';
+import path from 'path';
+import { glob } from 'glob';
+import { getHighlighter } from 'shiki';
+import serialize from 'serialize-javascript';
+import { Metadata } from '@/lib/types';
+import { v4 } from 'uuid';
+import { metadataSchema } from './schema';
 
 const params: string[] = process.argv.slice(2);
 let LOG_FILES = true;
 
-if (params[0] === "--dev" || params[0] === "-D") { 
+if (params[0] === '--dev' || params[0] === '-D') {
   LOG_FILES = false;
 }
 
@@ -25,7 +25,7 @@ type FileMap = Record<
 const main = async () => {
   const BASE_DIR = path.resolve(`${process.cwd()}/data`);
 
-  const files = await glob(BASE_DIR + "/components/**/*.{jsx,tsx,html,vue}");
+  const files = await glob(BASE_DIR + '/components/**/*.{jsx,tsx,html,vue}');
   const data: string[] = [];
   const names: string[] = [];
   const map: FileMap = {};
@@ -35,33 +35,33 @@ const main = async () => {
   for (const file of files) {
     const dirName = path.dirname(file);
 
-    if (path.basename(file) === "info.tsx") {
-      if (LOG_FILES) { 
-        console.log("../data" + file.replace(BASE_DIR, ""));
+    if (path.basename(file) === 'info.tsx') {
+      if (LOG_FILES) {
+        console.log('../data' + file.replace(BASE_DIR, ''));
       }
       data.push(
-        `import ${"n" + i} from '@/data${file
-          .replace(BASE_DIR, "")
-          .replace(/\.[^/.]+$/, "")}';`
+        `import ${'n' + i} from '@/data${file
+          .replace(BASE_DIR, '')
+          .replace(/\.[^/.]+$/, '')}';`
       );
 
       const { default: Comp } = (await import(
-        `@/data${file.replace(BASE_DIR, "")}`
+        `@/data${file.replace(BASE_DIR, '')}`
       )) as { default: Metadata };
 
-      const res = metadataSchema.safeParse(Comp);
-      if (!res.success) {
-        console.log(`Error while parsing metadata: ${file}`);
-        console.log(res.error.flatten());
-        process.exit(1);
-      }
+      // const res = metadataSchema.safeParse(Comp);
+      // if (!res.success) {
+      //   console.log(`Error while parsing metadata: ${file}`);
+      //   console.log(res.error.flatten());
+      //   process.exit(1);
+      // }
 
       categories.add(Comp.category);
 
       if (!map[dirName]) {
-        map[dirName] = { files: [], importId: "n" + i };
+        map[dirName] = { files: [], importId: 'n' + i };
       } else {
-        map[dirName] = { ...map[dirName], importId: "n" + i };
+        map[dirName] = { ...map[dirName], importId: 'n' + i };
       }
     } else {
       if (!map[dirName]) {
@@ -75,8 +75,8 @@ const main = async () => {
   }
 
   const highlighter = await getHighlighter({
-    themes: ["vitesse-dark"],
-    langs: ["typescript"],
+    themes: ['vitesse-dark'],
+    langs: ['typescript'],
   });
 
   for (const dd of Object.entries(map)) {
@@ -90,31 +90,31 @@ const main = async () => {
 
     for (const file of files) {
       const fileContent = fs.readFileSync(file.substring(1, file.length - 1), {
-        encoding: "utf-8",
+        encoding: 'utf-8',
       });
 
       const html = highlighter.codeToHtml(fileContent, {
-        lang: "typescript",
-        theme: "vitesse-dark",
+        lang: 'typescript',
+        theme: 'vitesse-dark',
       });
 
       contents.push(serialize(html));
     }
     names.push(
-      `{id: "${v4()}", metadata: ${importId}, code: [${contents.reverse().join(
-        ",\n"
-      )}], files : [${files.reverse().join(",\n")}]}`
+      `{id: "${v4()}", metadata: ${importId}, code: [${contents
+        .reverse()
+        .join(',\n')}], files : [${files.reverse().join(',\n')}]}`
     );
   }
 
-  data.push(`export const components = [${names.join(",\n")}]`);
+  data.push(`export const components = [${names.join(',\n')}]`);
   data.push(
     `export const categories = ${JSON.stringify(Array.from(categories).sort())}`
   );
 
   fs.writeFileSync(
-    path.resolve("./lib/emitter/components.ts"),
-    data.join("\n")
+    path.resolve('./lib/emitter/components.ts'),
+    data.join('\n')
   );
 };
 
