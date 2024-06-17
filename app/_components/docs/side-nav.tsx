f180bbd135be182709b34570c41ba0f541edf1a0
Pranjal Butola <pranjalbutola12@gmail.com>
2024-06-17 20:07:57 +0530

    fix font size and button hover effect

--- app/_components/docs/side-nav.tsx
+++ app/_components/docs/side-nav.tsx
@@ -1,12 +1,12 @@
-"use client";
+'use client';
 
-import { useParams, usePathname } from "next/navigation";
-import { cn } from "@/utils/cn";
-import { categories } from "@/lib/emitter/components";
-import Link from "next/link";
-import { ClassNameValue } from "tailwind-merge";
-import { transformCategory } from "@/utils";
-import { contents } from "@/lib/emitter/docs";
+import { useParams, usePathname } from 'next/navigation';
+import { cn } from '@/utils/cn';
+import { categories } from '@/lib/emitter/components';
+import Link from 'next/link';
+import { ClassNameValue } from 'tailwind-merge';
+import { transformCategory } from '@/utils';
+import { contents } from '@/lib/emitter/docs';
 
 type SideNavProps = {
   className?: ClassNameValue;
@@ -18,16 +18,16 @@ const SideNav = ({ className, input }: SideNavProps) => {
   const pathname = usePathname();
 
   return (
-    <div className={cn("sticky top-0 h-max pt-10 shrink-0", className)}>
+    <div className={cn('sticky top-0 h-max pt-10 shrink-0', className)}>
       {input}
-      <div className="absolute w-full h-10 bg-gradient-to-b from-[#141414] to-transparent"></div>
-      <div className="max-h-[85vh] overflow-x-hidden overflow-y-hidden hover:overflow-y-auto pl-3 midui-scroll">
-        <h2 className="mt-8 font-semibold text-[20px] mb-3">Documentation</h2>
+      <div className='absolute w-full h-10 bg-gradient-to-b from-[#141414] to-transparent'></div>
+      <div className='max-h-[85vh] overflow-x-hidden overflow-y-hidden hover:overflow-y-auto pl-3 midui-scroll'>
+        <h2 className='mt-8 font-semibold text-[18px] mb-3'>Documentation</h2>
         <div
           className={cn(
-            "items-start justify-center flex flex-col",
-            "py-[10px] gap-[12px]",
-            "text-[#B2B2B2] text-[16px]"
+            'items-start justify-center flex flex-col',
+            'py-[10px] gap-[12px]',
+            'text-[#B2B2B2] text-[16px]'
           )}
         >
           {contents.map(({ slugAsParams, name }) => (
@@ -35,8 +35,8 @@ const SideNav = ({ className, input }: SideNavProps) => {
               href={`/docs/${slugAsParams}`}
               key={slugAsParams}
               className={cn(
-                pathname === `/docs/${slugAsParams}` && "text-[#0D92FC]",
-                "transition ease-in-out delay-100 hover:-translate-x-1 hover:text-[#0D92FC]"
+                pathname === `/docs/${slugAsParams}` && 'text-[#0D92FC]',
+                'transition ease-in-out delay-100 hover:-translate-x-1 hover:text-[#0D92FC] text-sm'
               )}
             >
               {name}
@@ -44,13 +44,13 @@ const SideNav = ({ className, input }: SideNavProps) => {
           ))}
         </div>
 
-        <h2 className="mt-8 font-semibold text-[20px] mb-3">Components</h2>
+        <h2 className='mt-8 font-semibold text-[18px] mb-3'>Components</h2>
 
         <div
           className={cn(
-            "items-start justify-center flex flex-col",
-            "py-[10px] gap-[12px]",
-            "text-[#B2B2B2] text-[16px]"
+            'items-start justify-center flex flex-col',
+            'py-[10px] gap-[12px]',
+            'text-[#B2B2B2] text-[16px]'
           )}
         >
           {categories.map((c) => (
@@ -58,14 +58,14 @@ const SideNav = ({ className, input }: SideNavProps) => {
               href={`/docs/components/${c}`}
               key={c}
               className={cn(
-                category === c && "text-[#0D92FC]",
-                "transition ease-in-out delay-100 hover:-translate-x-1 hover:text-[#0D92FC]"
+                category === c && 'text-[#0D92FC]',
+                'transition ease-in-out delay-100 hover:-translate-x-1 hover:text-[#0D92FC] text-sm'
               )}
             >
               {transformCategory(c)}
             </Link>
           ))}
-          <div className="invisible mt-5"></div>
+          <div className='invisible mt-5'></div>
         </div>
       </div>
     </div>
--- data/components/button/1/example.tsx
+++ data/components/button/1/example.tsx
@@ -1,19 +1,19 @@
 export default function Button() {
   return (
     <>
-      <button className="bg-[#1565C0] font-poppins rounded-[6px] px-[24px] py-[10px]">
+      <button className='bg-[#1565C0] hover:bg-[#1565c0e1] transition-colors font-poppins rounded-[6px] px-[16px] py-[8px] ease-in-out'>
         Click Me
       </button>
 
-      <button className="bg-[#0D47A1] font-poppins rounded-[6px] px-[24px] py-[10px]">
+      <button className='bg-[#0D47A1] hover:bg-[#0d48a1e0] tranisiton-colors font-poppins rounded-[6px] px-[16px] py-[8px] '>
         Click Me
       </button>
 
-      <button className="bg-transparent font-poppins rounded-[6px] px-[24px] py-[10px] border-2 border-[#27272A]">
+      <button className='bg-transparent hover:bg-[#27272A] transition-colors font-poppins rounded-[6px] px-[16px] py-[8px] border-2 border-[#27272A]'>
         Click Me
       </button>
 
-      <button className="bg-transparent font-poppins rounded-[6px] px-[24px] py-[10px] hover:bg-[#212121] transition-colors">
+      <button className='bg-transparent font-poppins rounded-[6px] px-[16px] py-[8px] hover:bg-[#27272A] transition-colors'>
         Click Me
       </button>
     </>
