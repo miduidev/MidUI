694b010f5df1b13bb3f2aa8eadeb74d2baede592
Pranjal Butola <pranjalbutola12@gmail.com>
2024-06-17 20:43:04 +0530

    added pulse to hero gradient

--- app/_components/docs/code-block.tsx
+++ app/_components/docs/code-block.tsx
@@ -1,13 +1,13 @@
-"use client";
+'use client';
 
-import { cn } from "@/utils/cn";
-import path from "path-browserify";
-import { useMemo, useState } from "react";
-import CodeSelect from "./code-select";
-import { useConfigStore } from "@/store/config";
-import CodeDropdown from "./code-dropdown";
-import { CodeBracketIcon } from "@heroicons/react/24/outline";
-import CopyToClipboard from "./copy-to-clipboard";
+import { cn } from '@/utils/cn';
+import path from 'path-browserify';
+import { useMemo, useState } from 'react';
+import CodeSelect from './code-select';
+import { useConfigStore } from '@/store/config';
+import CodeDropdown from './code-dropdown';
+import { CodeBracketIcon } from '@heroicons/react/24/outline';
+import CopyToClipboard from './copy-to-clipboard';
 
 type CodeBlockProps = {
   files: string[];
@@ -22,47 +22,54 @@ export const CodeBlock = ({ files, codes }: CodeBlockProps) => {
       if (path.extname(files[i]) === `.${lang}`) {
         return i;
       }
-  }, [lang]);
+  }, [lang, files]);
 
   return (
-    <div className="flex flex-col">
+    <div className='flex flex-col'>
       <div
         className={cn(
-          "rounded-t-[10px] bg-[#121212] px-[20px]",
-          "py-[12px] text-[#FFFDFD] font-ibm",
-          "flex items-center justify-between"
+          'rounded-t-[10px] bg-[#121212] px-[20px]',
+          'py-[12px] text-[#FFFDFD] font-ibm',
+          'flex items-center justify-between'
         )}
       >
-        <div className="flex items-center space-x-1">
-          <CopyToClipboard code={codes[activeIndex||0]} />
-          <p className="font-medium text-[12px]">
+        <div className='flex items-center space-x-1'>
+          <CopyToClipboard code={codes[activeIndex || 0]} />
+          <p className='font-medium text-[12px]'>
             {path.basename(files[activeIndex ?? 0])}
           </p>
         </div>
 
-        <div className="flex items-center gap-[10px] justify-center">
+        <div className='flex items-center gap-[10px] justify-center'>
           <CodeSelect files={files} activeIndex={activeIndex} />
           <CodeDropdown files={files} activeIndex={activeIndex} />
         </div>
       </div>
 
-      <div style={{maxHeight : view?"":"300px"}} className="relative flex flex-col p-[16px] bg-[#121212] rounded-[10px] rounded-t-none overflow-hidden">
+      <div
+        style={{ maxHeight: view ? '' : '300px' }}
+        className='relative flex flex-col p-[16px] bg-[#121212] rounded-[10px] rounded-t-none overflow-hidden'
+      >
         <div
           dangerouslySetInnerHTML={{
             __html: codes[activeIndex ?? 0],
           }}
         ></div>
-        { 
-          !view &&
-          <div className="absolute bottom-0 w-full h-32 bg-gradient-to-b from-transparent to-[#141414]">
-            <div className="flex w-full justify-center items-end h-full">
-                <button className="text-sm flex items-center space-x-1" onClick={() => { setView(true) }}>
-                  <CodeBracketIcon className="size-4" />
-                  <span>View Source</span>
+        {!view && (
+          <div className='absolute bottom-0 w-full h-32 bg-gradient-to-b from-transparent to-[#141414]'>
+            <div className='flex w-full justify-center items-end h-full'>
+              <button
+                className='text-sm flex items-center space-x-1'
+                onClick={() => {
+                  setView(true);
+                }}
+              >
+                <CodeBracketIcon className='size-4' />
+                <span>View Source</span>
               </button>
             </div>
           </div>
-        }
+        )}
       </div>
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,24 @@
-import BrowseButton from "./_components/browse-button";
-import DisplayComponents from "./_components/display-components";
-import TailwindBadge from "./_components/tailwind-badge";
+import BrowseButton from './_components/browse-button';
+import DisplayComponents from './_components/display-components';
+import TailwindBadge from './_components/tailwind-badge';
 
 export default function Home() {
   return (
-    <main className="flex-1 flex justify-center md:justify-between px-10 h-full items-center mb-20 container mx-auto relative text-center">
-      <div className="gradient-06 -z-40"></div>
-      <div className="flex flex-col text-5xl md:text-7xl font-bold justify-center leading-tight">
+    <main className='flex-1 flex justify-center md:justify-between px-10 h-full items-center mb-20 container mx-auto relative text-center'>
+      <div className='gradient-06 -z-40'></div>
+
+      <div className='flex flex-col text-5xl md:text-7xl font-bold justify-center leading-tight'>
         <TailwindBadge />
-        <h1 className="opacity-90 tracking-tight">Free modern</h1>
-        <h1 className="opacity-90 tracking-tight">Open source</h1>
-        <h1 className="bg-gradient-to-r animated-hero-text-bg from-[#00D4FF] via-[#00D4FF] to-[#068BD4] bg-clip-text text-transparent tracking-tight">
-          <div className="flex flex-col w-fit">
+        <h1 className='opacity-90 tracking-tight'>Free modern</h1>
+        <h1 className='opacity-90 tracking-tight'>Open source</h1>
+        <h1 className='bg-gradient-to-r animated-hero-text-bg from-[#00D4FF] via-[#00D4FF] to-[#068BD4] bg-clip-text text-transparent tracking-tight'>
+          <div className='flex flex-col w-fit'>
             <span>Tailwindcss</span>
-            <div className="w-full animated-hero-text-bg h-1.5 bg-gradient-to-r from-[#00D4FF] via-[#00D4FF] to-[#068BD4]"></div>
+            <div className='w-full animated-hero-text-bg h-1.5 bg-gradient-to-r from-[#00D4FF] via-[#00D4FF] to-[#068BD4]'></div>
           </div>
-          <div className="flex flex-col w-fit">
-            <span className="text-center">Components</span>
-            <div className="w-full animated-hero-text-bg h-1.5 bg-gradient-to-r from-[#00D4FF] via-[#00D4FF] to-[#068BD4]"></div>
+          <div className='flex flex-col w-fit'>
+            <span className='text-center'>Components</span>
+            <div className='w-full animated-hero-text-bg h-1.5 bg-gradient-to-r from-[#00D4FF] via-[#00D4FF] to-[#068BD4]'></div>
           </div>
         </h1>
 
