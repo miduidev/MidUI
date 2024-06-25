1af9536f71413f82bf9980a5f8f09db9e762fc37
Pranjal Butola <pranjalbutola12@gmail.com>
2024-06-25 14:53:45 +0530

    added progress component

---
+++ data/components/progress/example.tsx
@@ -0,0 +1,22 @@
+'use client';
+import { useEffect, useState } from 'react';
+
+export function Progress() {
+  const [progress, setProgress] = useState(15);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setProgress((prev) => Math.min(100, prev + 10));
+    }, 1500);
+    return () => clearInterval(timer);
+  }, []);
+
+  return (
+    <div className='bg-gray-900 rounded-full flex items-start w-1/2 h-5 overflow-hidden'>
+      <div
+        className={`h-full w-full bg-[#1565C0] rounded-full transition-all`}
+        style={{ transform: `translateX(-${100 - progress}%)` }}
+      ></div>
+    </div>
+  );
+}
