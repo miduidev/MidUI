e4005b0555884509ba6cc19cc4658a523c8651d9
Pranjal Butola <pranjalbutola12@gmail.com>
2024-06-25 15:03:06 +0530

    fix code dropdown z index

--- app/_components/sheet.tsx
+++ app/_components/sheet.tsx
@@ -1,13 +1,13 @@
-"use client";
+'use client';
 
-import { categories } from "@/lib/emitter/components";
-import { contents } from "@/lib/emitter/docs";
-import { transformCategory } from "@/utils";
-import { cn } from "@/utils/cn";
-import Link from "next/link";
-import { useParams, usePathname } from "next/navigation";
-import { AnimatePresence, motion } from "framer-motion";
-import { XMarkIcon } from "@heroicons/react/24/outline";
+import { categories } from '@/lib/emitter/components';
+import { contents } from '@/lib/emitter/docs';
+import { transformCategory } from '@/utils';
+import { cn } from '@/utils/cn';
+import Link from 'next/link';
+import { useParams, usePathname } from 'next/navigation';
+import { AnimatePresence, motion } from 'framer-motion';
+import { XMarkIcon } from '@heroicons/react/24/outline';
 
 type SheetProps = {
   open: boolean;
@@ -28,7 +28,7 @@ const Sheet = ({ open, setOpen, input }: SheetProps) => {
             opacity: 1,
           }}
           exit={{ opacity: 0 }}
-          className="fixed w-screen h-screen inset-0 z-20 sheet overscroll-none"
+          className='fixed w-screen h-screen inset-0 z-[100] sheet overscroll-none'
           onClick={() => setOpen(false)}
           key={1}
         />
@@ -36,30 +36,30 @@ const Sheet = ({ open, setOpen, input }: SheetProps) => {
       {open && (
         <motion.div
           className={cn(
-            "w-[330px] bg-[#111111] pl-10 pr-20",
-            "py-14 border-r border-r-[#27272A] sheet-content",
-            "fixed h-screen inset-0 z-20 sheet-content overscroll-none overflow-y-scroll overflow-x-hidden"
+            'w-[330px] bg-[#111111] pl-10 pr-20',
+            'py-14 border-r border-r-[#27272A] sheet-content',
+            'fixed h-screen inset-0 z-[100] sheet-content overscroll-none overflow-y-scroll overflow-x-hidden'
           )}
           key={2}
           initial={{ x: -350 }}
           animate={{ x: 0 }}
-          transition={{ duration: 0.1, damping: 15, type: "spring" }}
+          transition={{ duration: 0.1, damping: 15, type: 'spring' }}
           exit={{ x: -350 }}
         >
           <XMarkIcon
             width={22}
             height={22}
-            className="absolute right-5 top-5"
+            className='absolute right-5 top-5'
             onClick={() => setOpen(false)}
           />
           {/* <Input className="w-lg mb-5" /> */}
           {input}
-          <h2 className="font-semibold text-2xl mb-3">Documentation</h2>
+          <h2 className='font-semibold text-2xl mb-3'>Documentation</h2>
           <div
             className={cn(
-              "items-start justify-center flex flex-col",
-              "py-[10px] gap-[12px]",
-              "text-[#B2B2B2] text-[16px]"
+              'items-start justify-center flex flex-col',
+              'py-[10px] gap-[12px]',
+              'text-[#B2B2B2] text-[16px]'
             )}
           >
             {contents.map(({ slugAsParams, name }) => (
@@ -70,8 +70,8 @@ const Sheet = ({ open, setOpen, input }: SheetProps) => {
                 href={`/docs/${slugAsParams}`}
                 key={slugAsParams}
                 className={cn(
-                  pathname === `/docs/${slugAsParams}` && "text-[#0D92FC]",
-                  "transition ease-in-out delay-100 hover:-translate-x-1 hover:text-[#0D92FC]"
+                  pathname === `/docs/${slugAsParams}` && 'text-[#0D92FC]',
+                  'transition ease-in-out delay-100 hover:-translate-x-1 hover:text-[#0D92FC]'
                 )}
               >
                 {name}
@@ -79,13 +79,13 @@ const Sheet = ({ open, setOpen, input }: SheetProps) => {
             ))}
           </div>
 
-          <h2 className="font-semibold text-2xl mb-3 mt-8">Components</h2>
+          <h2 className='font-semibold text-2xl mb-3 mt-8'>Components</h2>
 
           <div
             className={cn(
-              "items-start justify-center flex flex-col",
-              "py-[10px] gap-[12px]",
-              "text-[#B2B2B2] text-[16px]"
+              'items-start justify-center flex flex-col',
+              'py-[10px] gap-[12px]',
+              'text-[#B2B2B2] text-[16px]'
             )}
           >
             {categories.map((c) => (
@@ -96,8 +96,8 @@ const Sheet = ({ open, setOpen, input }: SheetProps) => {
                   setOpen(false);
                 }}
                 className={cn(
-                  category === c && "text-[#0D92FC]",
-                  "transition ease-in-out delay-100 hover:-translate-x-1 hover:text-[#0D92FC]"
+                  category === c && 'text-[#0D92FC]',
+                  'transition ease-in-out delay-100 hover:-translate-x-1 hover:text-[#0D92FC]'
                 )}
               >
                 {transformCategory(c)}
