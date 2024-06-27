c8268ad25d413c10826d65e704b52f98372ecca1
shivzee <notshivzee@gmail.com>
2024-06-27 16:47:19 +0530

    add radio component

---
+++ data/components/radio/1/example.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="flex space-x-2">
+        <input class="size-5 accent-[#1565C0]" type="radio" name="radio" id="radio_yes" />
+        <label htmlFor="radio_yes">Agree</label>
+    </div>
+    <div class="flex space-x-2">
+        <input class="size-5 accent-[#D32F2F]" type="radio" name="radio" id="radio_no" />
+        <label htmlFor="radio_no">Deny</label>
+    </div>
+</template>
---
+++ data/components/radio/1/info.tsx
@@ -0,0 +1,12 @@
+import { Metadata } from "@/lib/types";
+import Example from "./example";
+
+const metadata = {
+  title: "Radio",
+  description:
+    "A sleek Radio component designed with TailwindCSS for clear, responsive, and user-friendly selections.",
+  category: "radio",
+  component: Example,
+} satisfies Metadata;
+
+export default metadata;
