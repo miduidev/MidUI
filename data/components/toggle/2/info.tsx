8f33d7e37718c9474560d64b867ba7bf9a856506
Pranjal Butola <pranjalbutola12@gmail.com>
2024-06-27 14:34:47 +0530

    added toggle component

---
+++ data/components/toggle/2/info.tsx
@@ -0,0 +1,11 @@
+import { Metadata } from '@/lib/types';
+import { Toggle } from './example';
+
+const metadata: Metadata = {
+  title: 'Toggle Outline',
+  category: 'toggle',
+  component: Toggle,
+  description: 'A toggle switch with outline',
+};
+
+export default metadata;
