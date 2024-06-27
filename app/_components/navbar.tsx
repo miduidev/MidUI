94a16836bdeff50ba73fdc56261a383a640e592a
Pranjal Butola <pranjalbutola12@gmail.com>
2024-06-27 13:57:12 +0530

    modified hero components

--- app/_components/navbar.tsx
+++ app/_components/navbar.tsx
@@ -1,14 +1,14 @@
-"use client";
+'use client';
 
-import Link from "next/link";
-import Logo from "@/assets/logo.svg";
-import GithubLogo from "@/assets/github.svg";
-import { usePathname } from "next/navigation";
-import { Bars3Icon } from "@heroicons/react/24/outline";
-import Sheet from "./sheet";
-import React, { useEffect, useState } from "react";
-import { CommandMenu } from "./command-menu";
-import useOpenStore from "@/store/store";
+import Link from 'next/link';
+import Logo from '@/assets/logo.svg';
+import GithubLogo from '@/assets/github.svg';
+import { usePathname } from 'next/navigation';
+import { Bars3Icon } from '@heroicons/react/24/outline';
+import Sheet from './sheet';
+import React, { useEffect, useState } from 'react';
+import { CommandMenu } from './command-menu';
+import useOpenStore from '@/store/store';
 
 function Navbar({
   transparent = false,
@@ -30,43 +30,44 @@ function Navbar({
   return (
     <div
       className={`${
-        !transparent && pathname !== "/" && "border-b border-b-[#27272A]"
+        !transparent && pathname !== '/' && 'border-b border-b-[#27272A]'
       } ${
-        pathname.startsWith("/docs")
-          ? "bg-[#141414] sticky top-0 md:relative"
-          : "bg-transparent"
+        pathname.startsWith('/docs')
+          ? 'bg-[#141414] sticky top-0 md:relative'
+          : 'bg-transparent'
       } z-10`}
     >
-      <div className="w-full flex items-center container mx-auto justify-between px-5 py-4">
-        {pathname.startsWith("/docs") && (
+      <div className='w-full flex items-center container mx-auto justify-between px-5 py-4'>
+        {pathname.startsWith('/docs') && (
           <Bars3Icon
-            className="size-8 md:hidden"
+            className='size-8 md:hidden'
             onClick={() => setSheetOpen(true)}
           />
         )}
-        <Link href="/" className="flex items-center justify-center gap-2">
+        <Link href='/' className='flex items-center justify-center gap-2'>
           <Logo />
-          <h1 className="font-bold text-[22px] mt-1">MidUI</h1>
+          <h1 className='font-bold text-[22px] mt-1'>MidUI</h1>
         </Link>
 
-        <div className="flex items-center justify-center gap-8">
-          {!pathname.startsWith("/docs") && (
+        <div className='flex items-center justify-center gap-8'>
+          {!pathname.startsWith('/docs') && (
             <Link
-              href="/docs/getting-started"
-              className="text-[16px] transition ease-in-out delay-100 hover:-translate-x-1 hover:text-[#0D92FC]"
+              href='/docs/getting-started'
+              className='text-sm transition ease-in-out delay-100 hover:-translate-x-1 hover:text-[#0D92FC]'
             >
               Docs
             </Link>
           )}
+
           <Link
-            href="/contributors"
-            className="text-[16px] hidden md:block transition ease-in-out delay-100 hover:-translate-x-1 hover:text-[#0D92FC]"
+            href='/contributors'
+            className='text-sm hidden md:block transition ease-in-out delay-100 hover:-translate-x-1 hover:text-[#0D92FC]'
           >
             Sponsor
           </Link>
 
-          <Link target="_blank" href="https://github.com/miduidev/midui">
-            <GithubLogo className="transition ease-in-out delay-100 hover:-translate-x-1 cursor-pointer" />
+          <Link target='_blank' href='https://github.com/miduidev/midui'>
+            <GithubLogo className='transition ease-in-out delay-100 hover:-translate-x-1 cursor-pointer' />
           </Link>
         </div>
       </div>
--- data/components/input/3/example.tsx
+++ data/components/input/3/example.tsx
@@ -1,5 +1,5 @@
-"use client";
-import { useState } from "react";
+'use client';
+import { useState } from 'react';
 
 export function Input() {
   const [files, setFiles] = useState<Array<File>>([]);
@@ -10,8 +10,8 @@ export function Input() {
   }
 
   return (
-    <div className="flex flex-col gap-4 items-center">
-      <div className="rounded-[10px] bg-[#111111] border-2 border-[#27272A] px-5 py-3 gap-[10px] flex items-center justify-center focus-within:border-2 focus-within:border-[#1565C0] transition-colors min-w-32 hover:cursor-pointer text-white">
+    <div className='flex flex-col gap-4 items-center'>
+      <div className='rounded-[10px] bg-[#111111] border-2 border-[#27272A] px-5 py-3 gap-[10px] flex items-center justify-center focus-within:border-2 focus-within:border-[#1565C0] transition-colors min-w-32 hover:cursor-pointer text-white'>
         {files?.length == 0 ? (
           <UploadIcon />
         ) : files?.length == 1 ? (
@@ -20,19 +20,19 @@ export function Input() {
           <MulFileIcon />
         )}
         <label
-          htmlFor="fileBtn"
-          className="w-full bg-transparent outline-none hover:cursor-pointer"
+          htmlFor='fileBtn'
+          className='w-full bg-transparent outline-none hover:cursor-pointer'
         >
           {files?.length == 0
-            ? "Select File(s)"
+            ? 'Select File(s)'
             : files?.length == 1
             ? `${files[0].name}`
             : `${files?.length} files selected`}
         </label>
         <input
-          className="hidden"
-          type="file"
-          id="fileBtn"
+          className='hidden'
+          type='file'
+          id='fileBtn'
           multiple
           onChange={handleOnChange}
         />
@@ -41,21 +41,20 @@ export function Input() {
   );
 }
 
-
 function FileIcon() {
   return (
     <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
+      xmlns='http://www.w3.org/2000/svg'
+      fill='none'
+      viewBox='0 0 24 24'
       strokeWidth={1.5}
-      stroke="currentColor"
-      className="size-6"
+      stroke='currentColor'
+      className='size-6'
     >
       <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z"
+        strokeLinecap='round'
+        strokeLinejoin='round'
+        d='M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z'
       />
     </svg>
   );
@@ -64,17 +63,17 @@ function FileIcon() {
 function MulFileIcon() {
   return (
     <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
+      xmlns='http://www.w3.org/2000/svg'
+      fill='none'
+      viewBox='0 0 24 24'
       strokeWidth={1.5}
-      stroke="currentColor"
-      className="size-6"
+      stroke='currentColor'
+      className='size-6'
     >
       <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 0 1-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 0 1 1.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 0 0-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 0 1-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 0 0-3.375-3.375h-1.5a1.125 1.125 0 0 1-1.125-1.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H9.75"
+        strokeLinecap='round'
+        strokeLinejoin='round'
+        d='M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 0 1-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 0 1 1.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 0 0-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 0 1-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 0 0-3.375-3.375h-1.5a1.125 1.125 0 0 1-1.125-1.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H9.75'
       />
     </svg>
   );
@@ -83,17 +82,17 @@ function MulFileIcon() {
 function UploadIcon() {
   return (
     <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
+      xmlns='http://www.w3.org/2000/svg'
+      fill='none'
+      viewBox='0 0 24 24'
       strokeWidth={1.5}
-      stroke="currentColor"
-      className="size-6"
+      stroke='currentColor'
+      className='size-6'
     >
       <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5"
+        strokeLinecap='round'
+        strokeLinejoin='round'
+        d='M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5'
       />
     </svg>
   );
